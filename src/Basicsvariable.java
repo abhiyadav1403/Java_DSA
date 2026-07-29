999a3b356d813a13b6b999fac9619117f7334443
Abhishek Yadav <abhisheky19098@gmail.com>
2026-07-29 16:14:31 +0530

    Initial commit

---
+++ src/Basicsvariable.java
@@ -0,0 +1,5 @@
+public class Basicsvariable {
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
---
+++ src/conditionStatement.java
@@ -0,0 +1,11 @@
+public class conditionStatement {
+    public static void main(String[] args) {
+        int age = 13;
+        if( age> 18 ){
+            System.out.println("Eligibale for vote");
+        }
+        else {
+            System.out.println("Not Eligible for vote");
+        }
+    }
+}
