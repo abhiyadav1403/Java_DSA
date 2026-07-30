d780937b39949c8f1451dfb8c56f8f934ee20c81
Abhishek Yadav <abhisheky19098@gmail.com>
2026-07-30 20:44:56 +0530

    loops initial

---
+++ src/loops.java
@@ -0,0 +1,48 @@
+public class loops {
+    public static void main(String[] args) {
+//        for(int i =1; i<=5; i=i+1){
+//            System.out.println("print the value of the loop : " +i);
+//        }
+
+//        print even number:
+//        for(int i=2; i<=20; i=i+2){
+//            System.out.println("Even number :" +i);
+//        }
+//        for(int i = 1; i <= 20; i+= 2 ){
+//            System.out.println("Odd number : "+ i);
+//
+//        for(int i = 1; i <= 3; i++){
+//            for(int j = 1; j <= 3 ; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int k = 1; k<=10; k++){
+//            if(k==5){
+//                break;
+//            }
+//            System.out.println(k);
+//        }
+//        for(int k = 1; k<=10; k++){
+//            if(k==5){
+//                continue;
+//            }
+//            System.out.println(k);
+//        }
+//
+//
+        int i = 1;
+        while(i <= 10 ){
+            System.out.println(i);
+            i++;
+
+        }
+        int n = 1;
+        do{
+            System.out.println(n);
+            n++;
+        }while(n <= 5);
+
+
+    }
+}
