08997c566e7bd900c400ca4f74f81b9f241208d9
Abhishek Yadav <abhisheky19098@gmail.com>
2026-08-03 23:37:06 +0530

    initial commit

--- src/Pattern.java
+++ src/Pattern.java
@@ -80,27 +80,243 @@ public class Pattern {
 //            System.out.println();
 //        }
 
-        int n = 4;
-        for(int row =1; row <=n; row++){
-            for(int col=1 ; col <= 6; col++){
-                if(row ==1 || row==n){
-                    System.out.print("* ");
-                }
-                else{
-                    if(col == 1){
-                        System.out.print("* ");
-                    }
-                    else if(col == 6){
-                        System.out.print("* ");
-                    }
-                    else{
-                        System.out.print(" ");
-                    }
-                }
+//        int n = 4;
+//        for(int row =1; row <= n; row++){
+//            for(int col=1 ; col <= 6; col++){
+//                if(row ==1 || row == n){
+//                    System.out.print("* ");
+//                }
+//                else{
+//                    if(col == 1 || col == 6){
+//                        System.out.print("* ");
+//                    }
+//
+//                    else{
+//                        System.out.print("  ");
+//                    }
+//                }
+//            }
+//            System.out.println();
+//        }
+//        int m = 4;
+//
+//        for (int row = 1; row <= m; row++) {
+//            for (int col = 1; col <= 6; col++) {
+//                if (row == 1 || row == n || col == 1 || col == 6) {
+//                    System.out.print("* ");
+//                } else {
+//                    System.out.print("  "); // Two spaces
+//                }
+//            }
+//            System.out.println();
+//        }
+
+//      hollo right  Triangele
+
+//        int n = 10;
+//        for(int row = 1; row<=n; row++){
+////            for each row-> variable colums
+//            if(row ==1 || row ==2 || row ==n){
+//                for(int col=1; col<=row; col++){
+//                    System.out.print("* ");
+//                }
+//            }
+//            else{
+////                middel rows
+//
+//                System.out.print("* ");
+//
+////                (row-2) spaces
+//                for(int col=1; col<=(row-2); col++){
+//                    System.out.print("  ");
+//                }
+//                System.out.print("* ");
+//
+//            }
+////            move to next row
+//            System.out.println();
+//        }
+
+//        hollow solid pyramid
+
+//
+//        int n =4;
+//        for(int row =1; row<=n; row++){
+//            for(int col =1; col<=n-row; col++){
+//                System.out.print("  ");
+//            }
+////            part 2
+//            if(row ==1){
+//                for(int col=1; col<= 2*row -1; col++){
+//                    System.out.print("* ");
+//                }
+//
+//            }
+//            else{
+////                middle rows
+////                1*
+//                System.out.print("* ");
+////                2r-3 spaces
+//                for(int col=1; col<=2*row-3; col++){
+//                    System.out.print("  ");
+//                }
+////                1*
+//                System.out.print("* ");
+//
+//            }
+//            System.out.println();
+//        }
+//
+////        part 2
+//        for(int row=1; row<=(n-1); row++){
+////            for each row -> variable col
+//
+////            part1
+//            for(int col=1; col<=row; col++){
+//                System.out.print("  ");
+//            }
+////            part 2
+//            if(row ==(n-1)){
+//                System.out.print("* ");
+//            }
+//            else{
+////                remaining rows
+////                1*
+//                System.out.print("* ");
+//
+////                (2(n-r)-3) spaces
+//                for(int col=1; col<=(2*(n-row)-3); col++) {
+//                    System.out.print("  ");
+//                }
+////                1*
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+
+////Print the this question
+//*             *
+//* *         * *
+//* * *     * * *
+//* * * * * * * *
+//* * * * * * * *
+//* * *     * * *
+//* *         * *
+//*             *
+
+//        int n =4;
+//        for(int row=1; row<=n; row++){
+////            part 1
+//
+//            for(int col =1; col<=row; col++){
+//                System.out.print("* ");
+//            }
+////            part 2
+//            for(int col=1; col<=2*(n-row); col++){
+//                System.out.print("  ");
+//            }
+////            part 3
+//            for(int col=1; col<=row; col++){
+//                System.out.print("* ");
+//            }
+//
+//
+//            System.out.println( );
+//        }
+////
+//        for(int row=1; row<=n; row++){
+////         part 4
+//
+//            for(int col=1; col<=n-row+1; col++){
+//                System.out.print("* ");
+//            }
+////            part 5
+//            for(int col=1; col<=2*(row-1); col++){
+//                System.out.print("  ");
+//            }
+////            part 6
+//            for(int col=1; col<=n-row+1; col++){
+//                System.out.print("* ");
+//            }
+//
+//            System.out.println();
+//        }
+
+//print the number in triangele
+//        1
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5
+//
+//        int m=5;
+//        for(int row= 1; row <=m; row++){
+//            for(int col=1; col <= row; col++){
+//                System.out.print(col + " " );
+//            }
+//            System.out.println();
+//        }
+
+//        Print the number in Triangel
+//        1
+//        2 3
+//        4 5 6
+//        7 8 9 10
+//        11 12 13 14 15
+
+
+//        int n =5 ;
+//        int count =1;
+//        for(int row =1; row<=n; row++){
+//            for(int col =1; col<=row; col++){
+//                System.out.print(count +" ");
+//                count++;
+//            }
+//            System.out.println();
+//        }
+
+
+//        print the
+//        A
+//        A B
+//        A B C
+//        A B C D
+//        A B C D E
+
+//        int n =5;
+//        for(int row=1; row<=n; row++){
+//            for(int col=1; col<=row ; col++){
+//                int a = col;
+//                int b= ('A' -1);
+//                int ans = a+b;
+//                char finalAns = (char)ans;
+//                System.out.print(finalAns + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+//Print this question
+//        E
+////        E D
+////        E D C
+////        E D C B
+////        E D C B A
+
+
+        int n = 5;
+        for (int row = 1; row <= n; row++) {
+            for(int col=1; col<=row; col++){
+                int a =n-col;
+                int b = 'A';
+                int ans = a+b;
+                char finalAns = (char)ans;
+                System.out.print(finalAns +" ");
+
             }
             System.out.println();
         }
-
-
     }
 }
